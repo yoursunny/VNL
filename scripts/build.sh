feae594be5e3cce3bceddccfc64de48bc0ff95cd
Junxiao Shi <git@mail1.yoursunny.com>
2016-10-24 05:13:47 -0700

    scripts: fetch www contents

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,3 +9,16 @@ cd ..
 cd udpsum
 gcc -o udpsum --std=c99 udpsum.c
 cd ..
+
+mkdir -p www
+cd www
+if ! [[ -f index.php ]]; then
+  git init
+  git fetch --depth=1 https://github.com/yoursunny/VNL-www.git
+  git checkout FETCH_HEAD
+fi
+cd ..
+
+mkdir -p build build/www
+cp vnlsvc/vnlsvc udpsum/udpsum setlossy.php build/
+rsync --verbose --archive --delete -z --copy-links --exclude=.git www/ build/www
